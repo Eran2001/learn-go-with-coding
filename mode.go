da801993cba81df2a36d4132ea3b75f8da5549bc
M-E-Hasareli <eran.hasarali.2001@gmail.com>
2026-08-31 12:19:19 +0530

    add GET api

--- mode.go
+++ mode.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Item struct {
+    ID string `json:"id"`
+    Name string `json:"name"`
+    Price float64 `json:"price"`
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    items := []Item{
+        {ID: "1", Name: "Coffee", Price: 3.50},
+		{ID: "2", Name: "Tea", Price: 2.50},
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(items)
+}
 
 func main() {
-    fmt.Println("Running mode.go")
+    http.HandleFunc("/api/items", getItems)
+    http.ListenAndServe(":8000", nil)
+
+    fmt.Println("Server running at http://localhost:8000/api/items")
+
+    log.Println("Server running at http://localhost:8000/api/items")
+    log.Fatal(http.ListenAndServe(":8000", nil))
 }
